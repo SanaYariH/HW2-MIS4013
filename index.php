7e1d3430d15ff61a36084b4ddf627a82a4c13244
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-07 14:16:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,4 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
-
 <?php require_once("footer.php"); ?>
