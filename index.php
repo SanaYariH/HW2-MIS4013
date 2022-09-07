da713f2ecfaec26f1d6c779990fefeb8201e357c
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-07 14:24:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php require_once("header.php"); ?>
 
-<form action="welcome.php" method="post">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
+    
+    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
 
 <?php require_once("footer.php"); ?>
