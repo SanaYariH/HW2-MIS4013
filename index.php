0c42fac903c515220a91447d2d88d1a1cf7cace4
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 11:26:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Hello, This is Nicole's hwk2!</h1>
     <form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
