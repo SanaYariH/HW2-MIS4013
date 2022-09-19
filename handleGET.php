6211c377460b7bea8fa2857954e9588884b9a01c
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 12:24:30 -0500

    Update handleGET.php

--- handleGET.php
+++ handleGET.php
@@ -1,13 +1,10 @@
-<?php require_once("header.php"); ?>
-
 <html>
-  <body>
+<body>
 
-    <h1>GET Response</h1>
-    
+  <h1> GET RESPONSE </h1>
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-  </body>
+
+</body>
 </html>
