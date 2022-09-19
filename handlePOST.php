0e00b9cdba82584a2e3058b8ecf29e720e1c9d18
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 12:25:40 -0500

    Update handlePOST.php

--- handlePOST.php
+++ handlePOST.php
@@ -1,7 +1,9 @@
+
 <html>
   <body>
-    Welcome <?php echo $_POST["NAME"]; ?> <br>
-    Your Email Address is: <?php echo $_POST["EMAIL"]; ?>
+  
+    Welcome <?php echo $_POST["name"]; ?><br>
+    your email address is: <?php echo $_POST["email"]; ?>
     
   </body>
 </html>
