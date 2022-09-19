251559b291ba14b987973156a349ad412a09f36f
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 12:20:38 -0500

    Delete indexGET.php

--- indexGET.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("header.php"); ?>
-<form action="handleGET.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-<?php require_once("footer.php"); ?>
