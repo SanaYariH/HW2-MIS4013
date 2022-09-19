c0d10f0e219d002da8d6b46ccdbb5db536718ac6
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 12:49:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,24 @@
 <html lang = "en">
 <body>
   <h1> Welcome to Sana Yari's Homework 2!! </h1>
-  <h2> POST - Function </h2>
+  <p></p>
+  <p></p>
   
+  <h2> POST - Function </h2>
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
+  <p></p>
+  <p></p>
   
   <h3> GET - Function </h3>
   <form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
+    <p></p>
+    <p></p>
 </form>
 </form>
 </html>
