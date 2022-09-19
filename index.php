2a36466bda2adaff7fa4498ad1f85ab7eae3b18a
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-19 13:09:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,22 +7,20 @@
   <p style="color:red; text-align:center;">Enter your Name and Email Address please!</p>
   <br></br>
   
-  <p style="text-align:center;">
+  
   <h2 style="text-align:center;"> GET - Function </h2>
   <form action="welcome_get.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-    </p>
+  <p style="text-align:center;"> Name: <input type="text" name="name"></p><br>
+  <p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br>
+    <p style="text-align:center;"><input type="submit"></p>
     <br></br>
   
   <p style="text-align:center;">
   <h2 style="text-align:center;"> POST - Function </h2>
   <form action="handlepost.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-  </p>
+    <p style="text-align:center;"> Name: <input type="text" name="name"></p><br>
+    <p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br>
+    <p style="text-align:center;"><input type="submit"></p>
   <br></br>
   
   
