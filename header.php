958f5890d613a8d56f791c415201ee587c2d9316
SanaYariH <98046561+SanaYariH@users.noreply.github.com>
2022-09-26 14:02:46 -0500

    Update header.php

--- header.php
+++ header.php
@@ -9,20 +9,20 @@
   <body>
   <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">HomeWork 2 - MIS 4013</a>
+    <a class="navbar-brand" href="index.php">HomeWork 2 - MIS 4013</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="index.php">Features</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="index.php">Pricing</a>
         </li>
       </ul>
     </div>
